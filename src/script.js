a8e4737497384be3a869ec7a8a40a285e1247571
Tiffany Ni <ni.tiffany130@gmail.com>
2024-06-04 14:17:53 -0400

    implement slider

--- src/script.js
+++ src/script.js
@@ -36,22 +36,63 @@ document.addEventListener("DOMContentLoaded", () => {
       img.src = src;
       img.alt = alt;
       img.onclick = function () {
-        expandImage(this);
+        expandImage(this, idx, row.indexOf([src, alt]));
       };
 
       column.appendChild(img);
       imageContainer.appendChild(column);
     });
   });
+  window.picsArray = picsArray;
 });
 
-function expandImage(imgs) {
+let currentRowIndex = 0;
+let currentImgIndex = 0;
+
+function expandImage(imgs, rowIndex, imgIndex) {
   // Get the expanded image and text
   var expandImg = document.getElementById("expandedImg");
 
   // Use the same src in the expanded image as the image being clicked on from the grid
   expandImg.src = imgs.src;
+  expandImg.alt = imgs.alt;
 
   // Show the container element (hidden with CSS)
   expandImg.parentElement.style.display = "block";
+
+  //update the current image indices
+  currentRowIndex = rowIndex;
+  currentImgIndex = imgIndex;
+}
+
+function nextImage() {
+  if (currentImgIndex < window.picsArray[currentRowIndex].length - 1) {
+    currentImgIndex++;
+  } else if (currentRowIndex < window.picsArray.length - 1) {
+    currentRowIndex++;
+    currentImgIndex = 0;
+  } else {
+    currentRowIndex = 0;
+    currentImgIndex = 0;
+  }
+
+  //display the new image
+  const [src, alt] = window.picsArray[currentRowIndex][currentImgIndex];
+  expandImage({ src, alt }, currentRowIndex, currentImgIndex);
+}
+
+function prevImage() {
+  if (currentImgIndex > 0) {
+    currentImgIndex--;
+  } else if (currentRowIndex > 0) {
+    currentRowIndex--;
+    currentImgIndex = window.picsArray[currentRowIndex].length - 1;
+  } else {
+    currentRowIndex = window.picsArray.length - 1;
+    currentImgIndex = window.picsArray[currentRowIndex].length - 1;
+  }
+
+  //display the new image
+  const [src, alt] = window.picsArray[currentRowIndex][currentImgIndex];
+  expandImage({ src, alt }, currentRowIndex, currentImgIndex);
 }
